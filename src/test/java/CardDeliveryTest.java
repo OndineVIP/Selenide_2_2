39d321e7c71d47cb515021e3c909316d54439ee1
Ondine <ondinevika@gmail.com>
2023-07-06 16:20:05 +0300

    change tests

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -15,18 +15,19 @@ public class CardDeliveryTest {
     private String generateDate(int addDays, String pattern) {
         return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
     }
+
     @Test
     void shouldDeliveryCard() {
         open("http://localhost:9999");
 
-        $("[data-test-id='city'] input").setValue("Казань");
-        $("[data-test-id='date'] input").doubleClick().sendKeys(generateDate(4, "dd.MM.yyyy"));
-        $("[data-test-id='name'] input").setValue("Ахматова Анна");
-        $("[data-test-id='phone'] input").setValue("+79011122333");
+        $("[data-test-id=city] input").setValue("Казань");
+        $("[data-test-id=date] input").doubleClick().sendKeys(generateDate(3, "dd.MM.yyyy"));
+        $("[data-test-id=name] input").setValue("Ахматова Анна");
+        $("[data-test-id=phone] input").setValue("+79011122345");
         $(withText("Успешно")).shouldBe(Condition.hidden);
-        $("[data-test-id='agreement']").click();
+        $("[data-test-id=agreement]").click();
         $(".button").click();
-        $("[data-test-id=notification]").shouldBe(Condition.visible, Duration.ofSeconds(10));
+        $("[data-test-id=notification]").shouldBe(Condition.visible, Duration.ofSeconds(15));
         $("[data-test-id=notification]").shouldHave(Condition.text("Успешно!\n" +
                 "Встреча успешно забронирована на " + generateDate(4, "dd.MM.yyyy"))).shouldBe(Condition.visible);
     }
